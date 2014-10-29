6b776c2a2289dd4bb3ad6307a5db5b0eb3914dc2
Shaun Luttin <shaun@bigfont.ca>
2014-10-28 18:24:56 -0700

    Set profile in POST.

--- website-root/membership/index.php
+++ website-root/membership/index.php
@@ -17,16 +17,18 @@ if (isset($_GET['logout_admin'])) {
 	session_destroy();
 }
 
+$profile = "profile";
+$password = "password";
 
 if (isset($_POST['profile'])) {
 	$profile = $_POST['profile'];
 	$password = $_POST['password'];
-
+	
 	if ($profile == 'admin' && $password = 'sh33p!') {
 		//Admin has logged in
 		$_SESSION['admin'] = true;
 		header('location: admin.php');
-	}
+	}	
 
 	$sql = "select * from profiles where id = '$profile' && password = '$password'";
 	$result = mysql_query($sql);
@@ -54,30 +56,16 @@ $profiles = getProfiles();
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
     <link rel="stylesheet" type="text/css" href="css/styles.css" />
 
-
     <script src="js/modernizr.js"></script>
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
 
-
-
-    <script type="text/javascript">
-      /* <![CDATA[ */
-$(function() {
-
-
-});
-/*]]>*/
-    </script>
-
-
-
   </head>
   <body>
     <div id="container">
-
-      <h2 class="mb0">Salt Spring Studio Tour Registration</h2>
-      <p>We're in the process of making changes to the website.</p>
+                             
+      <h2 class="mb0">Salt Spring Studio Tour Registration</h2>			  
+      <p>We're in the process of making changes to the website.</p>            
       <p>We're also just learning where the previous website programmer put everything, so please be patient!</p>
       <p>
         Please log in below to manage your Studio Tour listing.
@@ -86,45 +74,43 @@ $(function() {
         If you have lost your login credentials, or your studio is not listed, please contact <a href="mailto:saltspringstudiotour@gmail.com">saltspringstudiotour@gmail.com</a>
       </p>
 
-      <fieldset>
-        <legend></legend>
-        <form class="form-horizontal" method="post" action = "index.php">
+      <form class="form-horizontal" method="post" action="index.php">
 
-          <div class="control-group">
-            <label class="control-label" for="profile">Studio Member</label>
+        <div class="control-group">
+          <label class="control-label" for="profile">Studio Member</label>
 
-            <div class="controls">
-              <select name="profile" id="profile" class="w400">
-                <?php foreach($profiles as $val) { $val = (object) $val; ?>
+          <div class="controls">
+            <select name="profile" id="profile" class="w400">
+              <?php foreach($profiles as $val) { $val = (object) $val; ?>
 
-                <option value=''
-                  <?=$val->id?>'>#<?=$val->tour_number?> <?=$val->studio_name?>
-                </option>
+              <option value='<?=$val->id?>'>
+				<span>#</span>
+				<span><?=$val->tour_number?><span>
+				<span><?=$val->studio_name?></span>
+              </option>
 
-                <?php } ?>
-                <option value="" disabled='disabled'></option>
-                <option value="admin">Administration</option>
-              </select>
-              <!--controls-->
-            </div>
-            <!--control-group-->
+              <?php } ?>
+              <option value="" disabled='disabled'></option>
+              <option value='admin'>Administration</option>
+            </select>
+            <!--controls-->
           </div>
+          <!--control-group-->
+        </div>
 
+        <div class="control-group">
+          <label class="control-label" for="inputPassword">Password</label>
+          <div class="controls">
+            <input type="password" id="inputPassword" name="password"> </div>
+        </div>
 
-
-
-          <div class="control-group">
-            <label class="control-label" for="inputPassword">Password</label>
-            <div class="controls">
-              <input type="password" id="inputPassword" name="password"> </div>
+        <div class="control-group">
+          <div class="controls">
+            <button type="submit" class="btn">Sign in</button>
           </div>
-          <div class="control-group">
-            <div class="controls">
-              <button type="submit" class="btn">Sign in</button>
-            </div>
-          </div>
-        </form>
-      </fieldset>
+        </div>
+
+      </form>
 
 
 
