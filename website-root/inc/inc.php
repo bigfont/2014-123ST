0ddd47ee9df08280991e565b54751ca1524a7a81
Shaun Luttin <shaun@bigfont.ca>
2015-01-19 16:09:17 -0800

    Put the database name in a variable.

--- website-root/inc/inc.php
+++ website-root/inc/inc.php
@@ -4,19 +4,19 @@ require_once ($_SERVER["DOCUMENT_ROOT"] . '/passwords.php');
 
 
 	session_start();
-  
-  $db = "studioWP"; 
+
+  $db = $DATABASE_NAME; 
 $host = $DATABASE_HOST;
 $user = $DATABASE_USER;
 $pass = $DATABASE_PASSWORD;
-		
+
 	$api = "ABQIAAAARF2-rsGK-9SnmPUa4Z9BVhSw_XUxS-1rSleO7ioMqdxOoBDP5RTZ04X1cFPYx3B3Ro84BuO_6yjmRA"; //192.168.1.104
 	$api = "ABQIAAAARF2-rsGK-9SnmPUa4Z9BVhSeE3pgkb2db23TzgDkBn5OBFKbYBSEcQUhAlg7ibm5Bjf02PXhf4qRdg"; //Webmechanic
 	$api = "ABQIAAAARF2-rsGK-9SnmPUa4Z9BVhTyPkaBe4qi3DVZ-qQx_QRDwHTmvhSsJRkOVln4HXeGNZboUU0fCnxvhw"; //Host Gator Temp
 	$api = "ABQIAAAARF2-rsGK-9SnmPUa4Z9BVhRGkzzA6C0Hd933s8y5YXSuhnqpQRTk2CXx4VdhTTi8_OixjEV1tPy3Cw"; //StudioTour.com
 	$conn = mysql_connect($host,$user,$pass) or die(mysql_error());
 	$d = mysql_select_db($db); // or die(mysql_error());
-	
+
 function admin_logged_in() {
 
 	if (isset($_SESSION['admin_id'])) {
@@ -24,15 +24,15 @@ function admin_logged_in() {
 	} else {
 		return FALSE;
 	}
-	
+
 }
 
 function admin_only() {
-	
+
 	if (!admin_logged_in()) {
 		header("location: index.php");
 	}
-	
+
 }
 
 
--- website-root/membership/inc/connect.php
+++ website-root/membership/inc/connect.php
@@ -6,7 +6,7 @@ session_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(-1);
-$db = "studioWP";
+$db = $DATABASE_NAME;
 $host = $DATABASE_HOST;
 $user = $DATABASE_USER;
 $pass = $DATABASE_PASSWORD;
